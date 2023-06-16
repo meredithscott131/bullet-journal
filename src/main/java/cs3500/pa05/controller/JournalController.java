1ee8c647332555666d2460dedfb4b90e7b494096
aspentabar <aspentabar@gmail.com>
2023-06-16 11:13:39 -0400

    added more views

--- src/main/java/cs3500/pa05/controller/JournalController.java
+++ src/main/java/cs3500/pa05/controller/JournalController.java
@@ -1,12 +1,17 @@
 package cs3500.pa05.controller;
 
 
+import cs3500.pa05.model.Calendar;
 
 /**
  * Represents the cs3500.pa05.controller for the journal
  */
 public class JournalController implements Controller {
 
+  //Fields:
+
+  //CalendarHandler handler;??????
+  Calendar calendar;
 
 
   /**
---
+++ src/main/java/cs3500/pa05/view/gui/DayView.java
@@ -0,0 +1,17 @@
+package cs3500.pa05.view.gui;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class DayView extends VBox {
+
+  //Fields:
+  Label title;
+  VBox input;
+
+
+
+
+
+
+}
