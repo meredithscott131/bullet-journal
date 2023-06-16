37ab6a80ee4b9fd49ced59be944142658be8d138
aspentabar <aspentabar@gmail.com>
2023-06-16 10:46:07 -0400

    added controller

---
+++ src/main/java/Controller/Controller.java
@@ -0,0 +1,15 @@
+package Controller;
+
+
+
+/**
+ * Interface to control the journal
+ */
+interface Controller {
+
+  /**
+   * Initializes the journal
+   */
+  void run();
+
+} //closes Controller interface
---
+++ src/main/java/Controller/JournalController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+
+
+/**
+ * Represents the controller for the journal
+ */
+public class JournalController implements Controller {
+
+
+
+  /**
+   * Initializes the journal
+   */
+  @Override
+  public void run() {
+
+  }
+}
