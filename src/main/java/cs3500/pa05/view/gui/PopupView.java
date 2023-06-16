707ac1f4db1a42a71a7a7e1b1d070536d4f1660a
LalCelik <114354209+LalCelik@users.noreply.github.com>
2023-06-16 18:25:24 -0400

    tried to make some button stuff 2

--- src/main/java/cs3500/pa05/view/gui/PopupView.java
+++ src/main/java/cs3500/pa05/view/gui/PopupView.java
@@ -4,13 +4,31 @@ package cs3500.pa05.view.gui;
 import cs3500.pa05.controller.JournalController;
 import cs3500.pa05.view.View;
 import java.io.IOException;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * Represents a interface journal GUI cs3500.pa05.view.view.
  */
 public class PopupView implements View {
+
+
+  @FXML
+  TextField nameTask;
+
+  @FXML
+  TextField decription;
+
+  @FXML
+  Button mondayButton;
+
+  @FXML
+  Button tuesdayButton;
+
+
 
   FXMLLoader loader;
 
