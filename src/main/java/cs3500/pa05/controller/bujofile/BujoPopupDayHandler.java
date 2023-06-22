819969345ee241e96f6e53c71b278080ef1593c4
meredithscott131 <scott.mer@northeastern.edu>
2023-06-21 20:12:00 -0400

    Added JavaDoc to the PopupDayHandler class

--- src/main/java/cs3500/pa05/controller/bujofile/BujoPopupDayHandler.java
+++ src/main/java/cs3500/pa05/controller/bujofile/BujoPopupDayHandler.java
@@ -4,18 +4,31 @@ import cs3500.pa05.model.DayWeek;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 
+/**
+ * Represents the handler for the bujo popup
+ */
 public class BujoPopupDayHandler implements EventHandler {
-
   private DayWeek dayWeek;
   private DayWeek startDay;
 
+  /**
+   * Instantiates a new Bujo popup handler for day selection.
+   *
+   * @param dayWeek  the day week
+   * @param startDay the start day
+   */
   public BujoPopupDayHandler(DayWeek dayWeek, DayWeek startDay) {
     this.dayWeek = dayWeek;
     this.startDay = startDay;
   }
 
+  /**
+   * Handles the user action event.
+   *
+   * @param event the action event
+   */
   @Override
   public void handle(Event event) {
-    startDay = dayWeek;
+    this.startDay = dayWeek;
   }
 }
